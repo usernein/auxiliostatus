9f830fb5f370b1436824731120db8050f021ec5e
Cezar H <29507335+usernein@users.noreply.github.com>
2020-04-26 01:14:27 -0300

    Use new exceptions' code

--- plugins/message/query.py
+++ plugins/message/query.py
@@ -19,7 +19,8 @@ async def on_query(client, message, lang):
         await message.reply(lang.status_long_result(status=status))
         await message.reply(lang.status_result(status=status))
         await message.reply(f'<code>{html.escape(json.dumps(status, indent=4))}</code>')
-    except auxilio.InvalidCode:
+    except auxilio.InvalidCode as e:
+        await message.reply(e.api_response)
         await message.reply(lang.error_invalid_code)
     except auxilio.InvalidResponse as e:
-        await message.reply(lang.error_invalid_response(error=str(e)))
+        await message.reply(lang.error_invalid_response(error=e.api_response))
