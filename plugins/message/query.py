3a65896983906dcd3cdc7bfa3ac4b0012dd7e960
Cezar H <29507335+usernein@users.noreply.github.com>
2020-04-25 23:09:17 -0300

    Edit strings, add usernein/auxilio

--- plugins/message/query.py
+++ plugins/message/query.py
@@ -1,12 +1,23 @@
+import json
 import re
 
 from pyrogram import Client, Filters
 from pyromod.helpers import ikb
 
+import auxilio
+
 @Client.on_message(Filters.regex(r'^(?P<cpf>([\._\-]*[0-9][\._\-]*){11})\|(?P<code>\w+)$'))
 async def on_query(client, message, lang):
-    print(message.matches)
     cpf = re.sub(r'\D', '', message.matches[0]['cpf'])
     code = message.matches[0]['code']
     
-    await message.reply(str([cpf, code]))
+    if not auxilio.valid_cpf(cpf):
+        return await message.reply(lang.error_invalid_cpf)
+    
+    try:
+        status = auxilio.status(cpf, code)
+        await message.reply('<code>'+json.dumps(status, indent=4)+'</code>')
+    except auxilio.InvalidCode:
+        await message.reply('Código inválido ou expirado.')
+    except auxilio.InvalidResponse as e:
+        await message.reply(f'Ocorreu um erro com a resposta da API: {e}')
