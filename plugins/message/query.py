f6249834dab28e85b92d84041c51f30d327f822d
Cezar H <29507335+usernein@users.noreply.github.com>
2020-04-25 21:12:59 -0300

    Debug regex result && escape pipe in regex

--- plugins/message/query.py
+++ plugins/message/query.py
@@ -3,8 +3,9 @@ import re
 from pyrogram import Client, Filters
 from pyromod.helpers import ikb
 
-@Client.on_message(Filters.regex(r'^(?P<cpf>([\._\-]*[0-9][\._\-]*){11})|(?P<code>\w+)$'))
+@Client.on_message(Filters.regex(r'^(?P<cpf>([\._\-]*[0-9][\._\-]*){11})\|(?P<code>\w+)$'))
 async def on_query(client, message, lang):
+    print(message.matches)
     cpf = re.sub(r'\D', '', message.matches[0]['cpf'])
     code = message.matches[0]['code']
     
